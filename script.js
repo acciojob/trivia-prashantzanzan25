db52eb23f8ae1ecc611b0e1a0c099488968d6a8e
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-11-06 19:19:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+it('blockquote should have the correct quotation.', () => {
+  cy.visit(baseUrl + "/main.html");
+  cy.get('blockquote').contains('New Delhi is the capital of India and an administrative district of NCT Delhi');
+});
+
